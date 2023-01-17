a86b013e748ab419b733f584cade224ecac3d335
Aditya Arun Rudrawar <arudrawa@iu.edu>
2023-01-17 17:32:41 -0500

    Create constant file

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const MED_DATA_ENDPOINT: string = "https://mmpyikxkcp.us-east-2.awsapprunner.com/v2/1NMfu1bEGNFcTYTFT-jCao_lSbFD8n0ti630iIpRj-hw/949267305"
+
+export const STRUCTURE_DATA_ENDPOINT : string = "https://www.ebi.ac.uk/ols/api/ontologies/uberon/terms?iri=http://purl.obolibrary.org/obo/"
