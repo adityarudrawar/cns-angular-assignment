820044ce8b2b0b0eb91e620f2565f8bc9231f423
Aditya Arun Rudrawar <arudrawa@iu.edu>
2023-01-17 17:33:17 -0500

    Add data service

---
+++ src/services/data-fetch.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MED_DATA_ENDPOINT, STRUCTURE_DATA_ENDPOINT } from '../constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataFetchService {
+
+  constructor( private _http:HttpClient) { }
+
+  getData(){
+    return this._http.get(MED_DATA_ENDPOINT);
+  }
+
+  getDatabyId(id: string){
+    // TODO: remove : from id to -
+    return this._http.get(STRUCTURE_DATA_ENDPOINT + id)
+  }
+}
